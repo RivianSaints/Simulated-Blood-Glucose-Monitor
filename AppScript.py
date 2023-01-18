caf5de90bc669e0bbe019f48d8095c3d8310309a
Scott Wright <77592598+RivianSaints@users.noreply.github.com>
2023-01-18 11:37:18 -0600

    Init Commit
    initial commit including script file and workspace

---
+++ AppScript.py
@@ -0,0 +1,23 @@
+##Author: Jake Lawrence
+## Date Created: 1/18/2023
+##This program will create a bluetooth connection with a microcontroller
+## and then using opensource GUI, allow the user to send a number
+
+import sys
+import getopt
+
+opts,args = getopt.getopt(sys.argv, "?hH")
+
+print("Hello World")
+
+print("test #1")
+
+
+
+
+
+
+
+
+
+
